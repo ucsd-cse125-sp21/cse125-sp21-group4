10419d154c6270b1384be0018012004e6f66b3ce
Eric Siu <esiu@ucsd.edu>
2021-04-08 22:50:25 -0700

    added amissing function

--- server/server.h
+++ server/server.h
@@ -1,3 +1,4 @@
 void cleanUpSocket(SOCKET* clientSocketPtr);
 DWORD WINAPI handlePlayerThread(LPVOID lpParam); 
 int checkBounds(int pos, int lower, int upper);
+void processInput(int assigned_player_id, CLIENT_INPUT input);
