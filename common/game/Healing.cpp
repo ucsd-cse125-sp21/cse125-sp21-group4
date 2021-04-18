0b9c6f383592c724a66597bee853f7084056f95c
Daryl <darylnak@noreply.com>
2021-04-18 13:47:44 -1000

    Rename Healing file and add getter for health. Add health amount in constructor.

---
+++ common/game/Healing.cpp
@@ -0,0 +1,17 @@
+#include "Healing.h"
+
+Heal::Heal(GridPosition position, Restriction r) {
+    setType(OBJECTIVE);
+    setRestriction(r);
+    setObjective(HEAL);
+    setPosition(position.x, position.y);
+    
+    if (r == R_FIGHTER)
+        h_amount = FIGHTER_MAX_HP / 50;
+    else
+        h_amount = (MONSTER_MAX_HP / 10); // 5 evolutions (health chunks), heal half a chunk
+}
+
+int Heal::getHealAmount() {
+    return h_amount;
+}
---
+++ common/game/Healing.h
@@ -0,0 +1,16 @@
+#ifndef _HEALING_
+#define _HEALING_
+
+#include "Objective.h"
+
+class Heal : public Objective {
+    private:
+        int h_amount; // amount to heal
+
+    public:
+        Heal(GridPosition position, Restriction r);
+        
+        int getHealAmount();
+};
+
+#endif
