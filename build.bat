221e4ed43abf03d9872d549c4b2bf750eae436ff
Eric Siu <esiu@ucsd.edu>
2021-04-20 01:48:02 -0700

    made changed to terminal client

--- build.bat
+++ build.bat
@@ -2,9 +2,6 @@
 ECHO Building client...
 g++ .\tests\test_windows_client.cpp .\client\CommunicationClient.cpp .\common\game\*.cpp -o .\client\client.exe -lWs2_32
 if NOT ["%errorlevel%"]==["0"] pause
-ECHO Building client with optimized network...
-g++ .\tests\test_windows_client_with_updates.cpp .\client\CommunicationClient.cpp .\common\game\*.cpp -o .\client\client_with_updates.exe -lWs2_32
-if NOT ["%errorlevel%"]==["0"] pause
 ECHO Building server...
 g++ .\server\*.cpp .\common\game\*.cpp -o .\server\server.exe -lWs2_32
 if NOT ["%errorlevel%"]==["0"] pause
