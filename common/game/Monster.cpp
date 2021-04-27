68041d01b0244fc4b380f7575e161bc21f160fe0
Amrita Kohli <amritakohli@live.com>
2021-04-26 19:56:12 -0700

    completed monster ranged attack and printing current player ID in terminal

--- common/game/Monster.cpp
+++ common/game/Monster.cpp
@@ -14,9 +14,30 @@ Monster::Monster(PlayerPosition position) : GamePlayer(position){
     setAttackDamage(MONSTER_ATTACK_DAMAGE);
 }
 
+// monster ranged attack
 void Monster::attack(Game* game) {
+    auto currentTime = std::chrono::steady_clock::now();
+    std::chrono::duration<float> duration = currentTime - lastAttackTime;
+    if (std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() 
+                                            < MONSTER_ATTACK_TIME_INTERVAL) {
+        return;
+    }
 
+    lastAttackTime = currentTime; // update the lastAttackTime as this attack
+
+    ProjectilePosition position = {
+        getPosition().x,
+        getPosition().y,
+    };
+
+    Projectile* p = new Projectile();
+    p->origin = position;
+    p->currentPosition = position; 
+    p->maxDistance = MONSTER_RANGED_ATTACK_DISTANCE; 
+    p->ownerType = getType();
+    p->type = MONSTER_RANGED; 
+    p->speed = MONSTER_RANGED_SPEED; //this?
+    p->direction = getFaceDirection();
+    p->damage = getAttackDamage();
+    game->projectiles.push_back(p);
 }
-
-
-
